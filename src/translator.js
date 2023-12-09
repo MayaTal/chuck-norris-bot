664f5fa1e28ac0283616ed3ccbf594324c2da567
Maya Tal <maya.tl123@gmail.com>
2023-12-09 20:31:20 +0200

    fix env

--- src/translator.js
+++ src/translator.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
 
 async function translateText(inputText, targetLanguage) {
+  const apiKey = process.env.AZURE_KEY;
   const endpoint = "https://api.cognitive.microsofttranslator.com";
   let params = new URLSearchParams();
   params.append("api-version", "3.0");
